630351b874280b99f2c5b4550ff15d411bdf1d35
Luiz Filipe Bartelega Penha <luizfpenha16@gmail.com>
2024-11-19 14:06:54 -0300

    tratamento HttpMessageNotREadable

--- bootcamp/src/main/java/squadra/com/br/bootcamp/handler/RestExceptionHandler.java
+++ bootcamp/src/main/java/squadra/com/br/bootcamp/handler/RestExceptionHandler.java
@@ -1,9 +1,11 @@
 package squadra.com.br.bootcamp.handler;
 
+import com.fasterxml.jackson.databind.JsonMappingException;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -45,7 +47,7 @@ public class RestExceptionHandler {
 
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     public ResponseEntity<ApiErrorFormat> handleMethodArgumentTypeMismatch(MethodArgumentTypeMismatchException ex) {
-
+        HttpStatus status = HttpStatus.BAD_REQUEST;
         String mensagem = String.format("O campo '%s' recebeu o valor '%s' que não é do tipo correto para este campo. O tipo correto é '%s'.",
                 ex.getName(),
                 ex.getValue(),
@@ -53,9 +55,30 @@ public class RestExceptionHandler {
 
         ApiErrorFormat apiErrorFormat = ApiErrorFormat.builder()
                 .mensagem(mensagem)
-                .status(HttpStatus.BAD_REQUEST.value())
+                .status(status.value())
                 .build();
 
-        return new ResponseEntity<>(apiErrorFormat, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(apiErrorFormat, status);
     }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ApiErrorFormat> handleHttpMessageNotReadable(HttpMessageNotReadableException ex) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        String mensagem = "Erro ao processar a requisição.";
+        String campoErro = "Campo desconhecido";
+
+        if (ex.getCause() instanceof JsonMappingException jsonEx) {
+            if (!jsonEx.getPath().isEmpty()) {
+                campoErro = jsonEx.getPath().getFirst().getFieldName();
+                mensagem = "Erro no campo '" + campoErro + "': valor inválido ou formato incorreto.";
+            }
+        }
+        ApiErrorFormat errorResponse = ApiErrorFormat.builder()
+                .mensagem(mensagem)
+                .status(status.value())
+                .build();
+        return new ResponseEntity<>(errorResponse, status);
+    }
+
+
 }
