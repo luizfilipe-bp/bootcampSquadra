80e3abe1d7cdb6b4ad0b335dc41e7ab3f6f1f1d7
Luiz Filipe Bartelega Penha <luizfpenha16@gmail.com>
2024-11-23 17:19:11 -0300

    retirada notacoes de validacao do vo

--- bootcamp/src/main/java/squadra/com/br/bootcamp/municipio/MunicipioVo.java
+++ bootcamp/src/main/java/squadra/com/br/bootcamp/municipio/MunicipioVo.java
@@ -1,19 +1,11 @@
 package squadra.com.br.bootcamp.municipio;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.hibernate.annotations.DynamicUpdate;
 
 @Data
 @Entity
-@NoArgsConstructor
-@AllArgsConstructor
 @DynamicUpdate
 @Table(name = "TB_MUNICIPIO")
 public class MunicipioVo {
@@ -24,17 +16,10 @@ public class MunicipioVo {
     @Column(name = "CODIGO_MUNICIPIO")
     private Long codigoMunicipio;
 
-    @NotNull(message = "O campo codigoUF não pode ser nulo.")
     @Column(name = "CODIGO_UF")
     private Long codigoUF;
 
-    @NotNull(message = "O campo nome não pode ser nulo.")
-    @Size(min = 1, max = 256, message = "O nome deve ter entre 1 e 256 caracteres.")
     private String nome;
 
-    @NotNull(message = "O campo status não pode ser nulo.")
-    @Min(value = 1, message = "o campo status deve ser 1 ou 2")
-    @Max(value = 2, message = "o campo status deve ser 1 ou 2")
     private Integer status;
-
 }
