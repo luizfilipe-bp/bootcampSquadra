a2aa4ed158100a3758db0232c3a7a312ad21ad86
Luiz Filipe Bartelega Penha <luizfpenha16@gmail.com>
2024-11-26 09:55:36 -0300

    adicionado notacoes

--- bootcamp/src/main/java/squadra/com/br/bootcamp/bairro/BairroRepository.java
+++ bootcamp/src/main/java/squadra/com/br/bootcamp/bairro/BairroRepository.java
@@ -1,7 +1,9 @@
 package squadra.com.br.bootcamp.bairro;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BairroRepository extends JpaRepository<BairroVo, Long> {
 
 }
